b85e254579f31e5ecac2389c2bbdf92c5b69731b
barbozaluiza-cell <barboza.luiza@escola.pr.gov.br>
2026-08-14 15:00:02 +0000

    Atualizar main.js

--- main.js
+++ main.js
@@ -7,3 +7,19 @@ const gameOver = document.querySelector('.gameOver');
 //Definição de audios
 const audioStart = new Audio('./sound/audio_theme.mp3');
 const audioGameOver = new Audio('./sound/audio_gameOver.mp3');
+
+// Variaveis de Controle do Jogo
+let gameInterval = mull;
+
+// Função para iniciar o jogo
+const starGame = () => {
+    pipe.classList.add('pipe-animation');
+    starButton.Style.display = 'none';
+    gameOver.style.display= 'none';
+    audioStart.currentTime = 0;
+    audioStart.play();
+
+        if(gameInterval) {
+        gameInterval = retInterval(gameLoop,10);
+    }
+};
