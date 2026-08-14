20c19538b2fb3ce2d3c5f5b25ece834fc78ca97b
barbozaluiza-cell <barboza.luiza@escola.pr.gov.br>
2026-08-14 13:45:22 +0000

    Atualização de mudanças

---
+++ main.js
@@ -0,0 +1,9 @@
+//Seleção de Elementos
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+const starButton = document.querySelector('.starButton'); 
+const restarButton = document.querySelector('.restarButton');
+const gameOver = document.querySelector('.gameOver'); 
+//Definição de audios
+const audioStart = new Audio('./sound/audio_theme.mp3');
+const audioGameOver = new Audio('./sound/audio_gameOver.mp3');
